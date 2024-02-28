66e5e1c9fe9f643482b54d9cb876aaf4698c5bc1
Mattheo992 <stepniak.matt@gmail.com>
2024-02-28 21:00:53 +0100

    updated

--- src/LessonStream/StudentTest.java
+++ src/LessonStream/StudentTest.java
@@ -10,10 +10,14 @@ public class StudentTest {
         students.add(new Student("Kamil", "Wąsik", 19, 2500.00));
         students.add(new Student("Adrian", "Surma", 20, 4999.00));
         students.add(new Student("Andrzej", "Boruta", 22, 5999.50));
-        students.stream()
-                .filter(n -> n.getName().startsWith("A")
-                        && n.getSalary() > 5000.00)
-                .toList()
-                .forEach(System.out::println);
+//        students.stream()
+//                .filter(n -> n.getName().startsWith("A")
+//                        && n.getSalary() > 5000.00)
+//                .forEach(System.out::println);
+//        students.stream()
+//                .map(n-> n.getName() + " " + n.getSurname())
+//                .forEach(System.out::println);
+        students.stream().sorted((s1, s2) -> s2.getAge() - s1.getAge()).
+                peek(s-> System.out.println(s.getAge() + " "+ s.getName() + " " + s.getSurname()));
     }
 }
